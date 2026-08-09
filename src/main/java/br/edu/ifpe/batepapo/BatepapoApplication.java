999392ba7860013050b64d488f7d3768befe8c91
YtalloGustavo <ytallogustavo.dev@gmail.com>
2026-08-09 14:27:00 -0300

    chore: scaffold Spring Boot 2.7.18 (Java 11) with maven wrapper
    Initial project scaffold generated from Spring Initializr: pom.xml, maven wrapper, gitignore, application properties and the vendor STOMP/SockJS client libraries.

---
+++ src/main/java/br/edu/ifpe/batepapo/BatepapoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.batepapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatepapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatepapoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpe/batepapo/BatepapoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.batepapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatepapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
